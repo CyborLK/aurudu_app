0965925dbebd1dd01315530139b52e9a63a4d635
Your Name <youremail@example.com>
2025-04-06 15:54:12 +0530

    build update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.aurudu_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973" // Updated to the required NDK version
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -20,10 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.aurudu_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,9 +29,17 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+
+    // ✅ Rename APK for release builds
+    applicationVariants.all {
+        if (buildType.name == "release") {
+            outputs.all {
+                val apkName = "Nekath_Seettuwa_2025_v${versionName}.apk"
+                (this as com.android.build.gradle.internal.api.BaseVariantOutputImpl).outputFileName = apkName
+            }
         }
     }
 }
